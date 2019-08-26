d2b758dc1dbaa4731730f770d166a7cdac70dd3e
ksygit <ksm6902@gmail.com>
2019-08-26 14:52:48 +0900

    Creating a Virtual Environment on NodeJS

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -6,10 +6,10 @@ const connectionOptions: ConnectionOptions = {
   synchronize: true,
   logging: true,
   entities: ["entities/**/*.*"],
-  host: process.env.DB_ENDPOINT || "localhost",
+  host: process.env.DB_ENDPOINT,
   port: 5432,
-  username: process.env.DB_USERNAME || "kapri",
-  password: process.env.DB_PASSWORD || "P@ssw0rd"
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
 };
 
 export default connectionOptions;
